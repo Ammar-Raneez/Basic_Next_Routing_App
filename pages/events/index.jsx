1b58b7ec5a5a96efff3658ba076c94b7ef131dae
ammar <ammarraneez@gmail.com>
2021-11-13 10:55:35 +0530

    feat: navigate to filtered events page

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,13 +1,24 @@
+import { useRouter } from 'next/router';
+
 import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 
 function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    // slug route ([...slug]) will consume unlimited amount of paths
+    // events/:eventId will navigate to [eventId]
+    // anything more that events/:eventId will navigate to [...slug]
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );
