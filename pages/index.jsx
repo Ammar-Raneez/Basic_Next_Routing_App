96faae89561a66033b2607d3c158b03ae7e5a329
ammar <ammarraneez@gmail.com>
2021-11-13 10:35:18 +0530

    feat: home page all events

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,11 +2,11 @@ import EventList from '../components/events/event-list';
 import { getFeaturedEvents } from '../dummy-data';
 
 function HomePage() {
-  const FEATURES_EVENTS = getFeaturedEvents();
+  const featuredEvents = getFeaturedEvents();
 
   return (
     <div>
-      <EventList items={FEATURES_EVENTS} />
+      <EventList items={featuredEvents} />
     </div>
   );
 }
