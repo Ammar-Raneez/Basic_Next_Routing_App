ecd8256f23116297e475e439e62203af5292db78
ammar <ammarraneez@gmail.com>
2021-11-13 10:47:14 +0530

    feat: add filter dropdowns

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -2,14 +2,25 @@ import Link from 'next/link';
 import classes from './button.module.css';
 
 function Button(props) {
+  if (props.link) {
+    return (
+      // to apply styles we need to create a manual anchor tag
+      // the anchor tag overrides the automatically added one by the Link componenr
+      // Every other functionality provided by the Link component is automatically added to the anchor
+      // Hence, we don't need to specify the anchors' href
+      <Link href={props.link}>
+        <a className={classes.btn}>{props.children}</a>
+      </Link>
+    );
+  }
+
   return (
-    // to apply styles we need to create a manual anchor tag
-    // the anchor tag overrides the automatically added one by the Link componenr
-    // Every other functionality provided by the Link component is automatically added to the anchor
-    // Hence, we don't need to specify the anchors' href
-    <Link href={props.link}>
-      <a className={classes.btn}>{props.children}</a>
-    </Link>
+    <button
+      className={classes.btn}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
   );
 }
 
